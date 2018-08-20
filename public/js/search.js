6b380a11037ef3f060ce7847f4215ae18300b59b
jakekasan <jake.kasan@gmail.com>
2018-08-20 22:18:25 +0100

    when returning to search, previous query loads with page

--- public/js/search.js
+++ public/js/search.js
@@ -1,3 +1,16 @@
+
+window.onload = function(){
+    // in case we've went back and the search bar has text
+
+    updateWindow();
+
+    const searchBar = document.getElementById("search");
+
+    searchBar.addEventListener("keyup",() => {
+        updateWindow();
+    });
+}
+
 const pageLoader = function(data){
     let searchResults = document.getElementById("searchResults");
     searchResults.innerHTML = "";
@@ -5,13 +18,13 @@ const pageLoader = function(data){
     for (let ind of data){
 
         console.log(ind);
-        console.log(ind["Type"]);
+        console.log(ind["type"]);
 
         let cardContainer = document.createElement("div");
         cardContainer.className = "col s12 m6 l4 xl2";
 
         let card = document.createElement("div");
-        card.className = "card";
+        card.className = "small card";
 
         let cardContent = document.createElement("div");
         cardContent.className = "card-content";
@@ -20,9 +33,9 @@ const pageLoader = function(data){
         cardTitle.className = "card-title";
         cardTitle.innerText = ind["type"];
 
-        let cardImage = document.createElement("img");
+        //let cardImage = document.createElement("img");
         //cardImage.className = "";
-        cardImage.src = "img/homer.png";
+        //cardImage.src = "img/homer.png";
 
         let cardImageContainer = document.createElement("div");
         cardImageContainer.className = "card-image";
@@ -37,43 +50,47 @@ const pageLoader = function(data){
         cardContent.appendChild(cardText);
         card.appendChild(cardContent);
         cardContainer.appendChild(card);
+
+        // cardContainer event listener to transfer to main view
+        cardContainer.addEventListener("click",() => {
+            window.location = `./view?id=${ind["id"]}`
+        });
         searchResults.appendChild(cardContainer);
     }
 }
 
-window.onload = function(){
-    const searchResultsContainer = document.getElementById("searchResults");
-    const searchBar = document.getElementById("search");
 
-    searchBar.addEventListener("keyup",(Element,Event) => {
-        let query = searchBar.value;
+const updateWindow = function(){
+    console.log("updating window");
+
+    let searchBar = document.getElementById("search");
+    let query = searchBar.value;
+
+    console.log(query);
+
+    if (query != ""){
+        //console.log("Query is not empty");
         
-        //console.log(query);
+        let params = query.split(" ").filter(x => { return (x != "") }) 
 
-        if (query != ""){
-            //console.log("Query is not empty");
-            
-            let params = query.split(" ").filter(x => { return (x != "") }) 
+        //console.log(params);
 
-            //console.log(params);
+        params = params.join("+");
 
-            params = params.join("+");
+        let url = new URL("http://localhost:8080/api/data/search");
+        url.search = new URLSearchParams({
+            q:params
+        });
 
-            let url = new URL("http://localhost:8080/api/data/search");
-            url.search = new URLSearchParams({
-                q:params
-            });
+        //console.log(url);
 
-            //console.log(url);
-
-            fetch(url)
-                .then(data => data.json())
-                .then(data => {
-                    //console.log(data);
-                    pageLoader(data);
-            });
-        };
-    });
-};
+        fetch(url)
+            .then(data => data.json())
+            .then(data => {
+                //console.log(data);
+                pageLoader(data);
+        });
+    };
+}
 
 
