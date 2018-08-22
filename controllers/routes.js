e6b45cac0ece0ba3c0e70fff1373c3c1da53de25
jakekasan <jake.kasan@gmail.com>
2018-08-22 18:54:20 +0100

    reworked the /edit route promise logic

--- controllers/routes.js
+++ controllers/routes.js
@@ -36,84 +36,66 @@ module.exports = function(app,db){
     app.get("/edit",(req,res) => {
         const rawData = db.getById(req.query.id);
         let url = new URL("http://localhost:8080/api/data");
+        console.log(`Fetching /edit for id ${req.query.id}`)
         url.search = new URLSearchParams({
             id:req.query.id
         });
         fetch(url)
             .then(data => data.json())
             .then(data => {
+                console.log(Object.keys(data));
                 let iterables = Object
                                     .keys(data)
                                     .map(key => { return data[key] })
-                                    .filter(item => { (item instanceof Array) });
+                                    .filter(item => { return (item instanceof Array) });
                 let nonIterables = Object
                                     .keys(data)
-                                    .map(key => { return data[key] })
-                                    .filter(item => { !(item instanceof Array) });
+                                    .map(key => {
+                                        let obj = {};
+                                        obj[key.toUpperCase()] = data[key];
+                                        return obj
+                                    })
+                                    .filter(item => { return !(Object.values(item)[0] instanceof Array) });
+                console.log("Iterables and nonIterables");
+                console.log(iterables);
+                console.log(nonIterables);
                 let promises = iterables
+                                .filter(item => { return (item.length > 0) })
                                 .map(item => {
                                     // TO-DO: make new promise for each element in each item !!!
-                                    console.log("Item in promises");
-                                    console.log(item);
+                                    //console.log("Making promises for array",item);
                                     let subPromises = item.map(subItem => {
-                                        let subUrl = new URL("https://localhost:8080/api/data");
+                                        //console.log(`Fetching info for id ${subItem}`);
+                                        let subUrl = new URL("http://localhost:8080/api/data");
                                         subUrl.search = new URLSearchParams({
-                                            id:subItem.id
+                                            id:subItem
                                         });
                                         return fetch(subUrl).then(data => data.json());
                                     });
-                                    return Promise.all(subPromises).then(values => {
-                                        console.log("Values in subPromise");
-                                        console.log(values);
-                                        return values
-                                    });
+                                    return Promise.all(subPromises)
                                 });
                 Promise.all(promises)
                                 .then(values => {
-                                    console.log("Values:");
-                                    console.log(values);
-                                    
                                     // make new object and render it
                                     finalObj = {};
+
+                                    for (let value of nonIterables){
+                                        finalObj[Object.keys(value)] = Object.values(value)[0];
+                                    }
+
                                     for (let value of values) {
                                         finalObj[value[0]["type"]] = value;
                                     }
-                                    console.log("Replying to client");
-                                    console.log(finalObj);
+
                                     //res.send("success!");
+                                    console.log(finalObj);
                                     res.render("edit",{data:finalObj})
                                 })
-                                .catch(e => console.log(e));
-                
-                
+                                .catch(e => {
+                                    console.log("Caught error in the last Promise.all()");
+                                    console.log(e);
+                                });
             });
-        // for (let key in rawData){
-        //     if (rawData.hasOwnProperty(key)){
-        //         let idList = rawData[key];
-        //         if (idList instanceof Array){
-        //             let promises = idList.map(item => {
-        //                 console.log("Item")
-        //                 console.log(item);
-        //                 let url = new URL("http://localhost:8080/api/data")
-        //                 url.search = new URLSearchParams({
-        //                     id:String(item)
-        //                 });
-        //                 return fetch(url).then(data => data.json());
-        //             });
-        //             Promise.all(promises).then(values => {
-        //                 //console.log(`setting ${key} as ${JSON.stringify(values)}`)
-        //                 rawData[key] = values;
-        //             }).catch(e => { console.log(e) });
-        //         }
-        //     }
-        // }
-        // if (rawData == false){
-        //     res.send("Not found!");
-        // } else {
-        //     console.log("/edit Data");
-        //     console.log(rawData);
-        //     res.render("edit",{data:rawData});
-        // }
     });
 
     app.get("/search",(req,res) => {
@@ -129,8 +111,12 @@ module.exports = function(app,db){
     app.get("/api/data",(req,res) => {
         //console.log(req.query);
         let result = db.get(req.query);
-        //console.log(result);
-        res.send(JSON.stringify(result));
+        if (result){
+            res.send(JSON.stringify(result));
+        } else {
+            res.send("");
+        }
+        
     });
 
     app.get("/api/data/id",(req,res) => {
