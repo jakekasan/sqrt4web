9547589bca22bdab26c4b81dc51b787f14424df9
jakekasan <jake.kasan@gmail.com>
2018-10-22 19:44:44 +0100

    wrote new tests for base controller

--- tests/controllers/base.controller.spec.js
+++ tests/controllers/base.controller.spec.js
@@ -15,74 +15,27 @@ const baseController = require("./../../controllers/base.controller");
 
 describe("base.controller",() => {
 
-    describe("controller structure",() => {
+    describe("structure",() => {
+        var bc;
+
+        beforeEach(() => {
+            bc = new (require("./../../controllers/base.controller"))();
+        });
 
         it("should be an object",() => {
-            const baseController = require("./../../controllers/base.controller");
-
-            baseController.should.be.an("object");
+            expect(bc).to.be.an("object");
         });
 
-        it("should have 'name' property", () => {
-            const baseController = require("./../../controllers/base.controller");
-
-            baseController.should.have.property("name");
-        });
-
-        it("name property should be equal to 'base.controller'",() => {
-            const baseController = require("./../../controllers/base.controller");
-
-            baseController.name.should.equal("base.controller");
-        });
-
-        it("should have 'run' property", () => {
-            const baseController = require("./../../controllers/base.controller");
-
-            baseController.should.have.property("run");
+        it("should have attribute 'run'", () => {
+            bc.should.have.property("run");
         });
 
         it("run property should be a function", () => {
-            const baseController = require("./../../controllers/base.controller");
-
-            baseController.run.should.be.a("function");
+            bc.run.should.be.a("function");
         });
 
-        it("run function should eventually call a function of res", () => {
-            const baseController = require("./../../controllers/base.controller");
-
-            var hasFunctionBeenRun = false;
-
-            let req = {
-                body:null,
-                method:null,
-                path:null
-            };
-
-            let res = {
-                render: function(template,data){
-                    return hasFunctionBeenRun = true;
-                },
-                json: function(data){
-                    return hasFunctionBeenRun = true;
-                }
-            }
-            baseController.run(req,res,next);
-
-            assert(hasFunctionBeenRun === true);
-        })
-
-        it("should have 'getContent' property", () => {
-            const baseController = require("./../../controllers/base.controller");
-
-            baseController.should.have.property("getContent");
+        it("run should throw an error if req,res or next are missing", () => {
+            assert.throw(bc.run);
         });
-
-        it("getContent property should be a function ", () => {
-            const baseController = require("./../../controllers/base.controller");
-
-            baseController.getContent.should.be.a("function");
-        });
-
-        it("")
     })
 })
