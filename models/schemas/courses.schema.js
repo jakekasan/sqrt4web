9369b2d81fa231b683436c8d5bfe3dcaa606e731
jakekasan <jake.kasan@gmail.com>
2018-10-22 21:07:19 +0100

    Courses schema

---
+++ models/schemas/courses.schema.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+module.exports = mongoose.Schema({
+    id: Number,
+    title: String,
+    description: String,
+    modules:[{
+        order: Number,
+        timeToComplete: Number // amount of minutes to complete
+    }]
+})
