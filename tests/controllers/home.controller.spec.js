5ac9c82d2e7f8c11a12d69b875b8c78e4d4db039
jakekasan <jake.kasan@gmail.com>
2018-10-22 19:46:01 +0100

    added home controller tests

---
+++ tests/controllers/home.controller.spec.js
@@ -0,0 +1,35 @@
+const expect = require("chai").expect;
+const should = require("chai").should();
+const assert = require("chai").assert;
+
+describe("Home Controller",() => {
+
+    describe("Basics",() => {
+        var hc;
+
+        beforeEach(() => {
+            hc = new (require("./../../controllers/home.controller"))();
+        });
+
+        it("should be an object", () => {
+            hc.should.be.an("object");
+        });
+
+        it("should have a property 'name'", () => {
+            hc.should.have.property("name");
+        });
+
+        it("name should be equal to 'Home Controller'", () => {
+            hc.name.should.equal("Home Controller");
+        });
+
+        it("should have a property 'run'",() => {
+            hc.should.have.property("run");
+        });
+
+        it("run should be a function", () => {
+            hc.run.should.be.a("function");
+        })
+
+    })
+})
