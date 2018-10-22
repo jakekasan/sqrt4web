75eaa5ca281ad14bdbee78602c8ac1f67f46c979
jakekasan <jake.kasan@gmail.com>
2018-10-22 20:09:29 +0100

    added base model class

---
+++ models/base.model.js
@@ -0,0 +1,25 @@
+class BaseModel {
+    constructor(){
+        // set schemas?
+    }
+
+    create(objectToCreate,callback){
+        // create object
+        callback(null,null)
+    }
+
+    retrieve(objectToCreate,callback){
+        // create object
+        callback(null,null)
+    }
+
+    update(objectToUpdate,updatedObject,callback){
+        // create object
+        callback(null,null)
+    }
+
+    delete(objectToDelete,callback){
+        // create object
+        callback(null,null)
+    }
+}
