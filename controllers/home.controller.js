ae2288262630b05461a37f3c0496b3c41100c208
jakekasan <jake.kasan@gmail.com>
2018-10-25 16:55:10 +0100

    filled in run and getContent functions

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,5 +1,5 @@
 const BaseController = require("./base.controller");
-
+const BaseView = require("./../views/base.view");
 
 class HomeController extends BaseController {
 
@@ -7,8 +7,40 @@ class HomeController extends BaseController {
         super("Home Controller");
     }
 
-    run(){
-        // nothing for now
+    run(req,res,next){
+        var self = this; // for scope
+
+        self.req = req;
+        self.res = res;
+        self.next = next;
+
+        if (this.checkLoggedStatus(self)){
+            this.getContent(self);
+        } else {
+            this.getContent(self);
+        }
+    }
+
+    checkLoggedStatus(self){
+        /*
+        
+            returns either true or false depending on if req has a valid user session with userdata
+        
+        */
+
+       return false // for now...
+    }
+
+    getContent(self){
+        // temporary data
+        data = {
+            title:"Welcome to SQRT4"
+        }
+
+        // get data and render template
+        let view = new BaseView("home",self.res)
+        self.content = data;
+        return view.render(self.content);
     }
 
 
