da7247605b2af4099bd14bde33196a6b95349c7b
jakekasan <jake.kasan@gmail.com>
2018-10-25 21:47:22 +0100

    added imports and mongoDB connection

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const Express = require("express");
 const app = Express();
 const routes = require("./routes");
+const config = require("./config/index")();
+const mongoose = require("mongoose");
 
 app.set("view engine","ejs");
 
@@ -8,7 +10,11 @@ app.use(Express.static("public"));
 
 //routes(app);
 
+let mongo = mongoose.connect(config.db.mongodb.address)
+
 
 app.listen(8080,() => {
     console.log("App is running");
+    console.log("Config settings:\n");
+    console.log(JSON.stringify(config,null,2));
 });
