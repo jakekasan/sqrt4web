d0bfc748aff9501c0e5cf483f7f0ed512fe8aa02
jakekasan <jake.kasan@gmail.com>
2019-01-15 15:05:54 +0000

    added debugging console.logs to run function

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -3,22 +3,32 @@ const BaseView = require("./../views/base.view");
 
 class HomeController extends BaseController {
 
-    constructor(){
+    constructor(debug){
         super("Home Controller");
+        this.debug = debug;
     }
 
     run(req,res,next){
+        console.log("Running run");
+        if (this.debug) {
+            console.log(`${Date.now()} : ${req.method} request recieved`);
+        }
         var self = this; // for scope
-
+        console.log("Set self");
         self.req = req;
         self.res = res;
         self.next = next;
+        self.mongo = self.req.mongo;
 
-        if (this.checkLoggedStatus(self)){
-            this.getContent(self);
-        } else {
-            this.getContent(self);
-        }
+        console.log("Running getContent");
+        this.getContent(self);
+
+
+        // if (self.checkLoggedStatus(self)){
+        //     self.getContent(self);
+        // } else {
+        //     self.getContent(self);
+        // }
     }
 
     checkLoggedStatus(self){
@@ -32,13 +42,14 @@ class HomeController extends BaseController {
     }
 
     getContent(self){
+        console.log("In get content");
         // temporary data
-        data = {
+        let data = {
             title:"Welcome to SQRT4"
         }
 
         // get data and render template
-        let view = new BaseView("home",self.res)
+        let view = new BaseView("main",self.res)
         self.content = data;
         return view.render(self.content);
     }
