30123c0540460763dbb1ed3c34698c20ec29620a
jakekasan <jake.kasan@gmail.com>
2019-01-15 15:04:17 +0000

    root route now calls home controller function

--- app.js
+++ app.js
@@ -4,17 +4,44 @@ const routes = require("./routes");
 const config = require("./config/index")();
 const mongoose = require("mongoose");
 
+// Controllers
+const HomeController = require("./controllers/home.controller");
+
+// Models
+const UsersModel = require("./models/users.model");
+
+// Views
+const BaseView = require("./views/base.view");
+
 app.set("view engine","ejs");
 
 app.use(Express.static("public"));
 
 //routes(app);
 
-let mongo = mongoose.connect(config.db.mongodb.address)
+mongoose.connect(config.db.mongodb.address,(err,conn) => {
+    var hc = new HomeController(true);
+
+    function databaseMiddleware(req,res,next){
+        req.mongo = conn;
+        next();
+    }
+
+    app.use(databaseMiddleware);
+
+    app.all("/",(req,res,next) => {
+        console.log("Req to '/'");
+        // res.render("main");
+        // return
+        console.log(hc);
+        hc.run(req,res,next);
+    });
+
+    app.listen(8080,() => {
+        console.log("App is running");
+        console.log("Config settings:\n");
+        console.log(JSON.stringify(config,null,2));
+    });
+})
 
 
-app.listen(8080,() => {
-    console.log("App is running");
-    console.log("Config settings:\n");
-    console.log(JSON.stringify(config,null,2));
-});
