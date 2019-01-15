683f0239dbaff5d4edfbb6af0431dba94b1a03d2
jakekasan <jake.kasan@gmail.com>
2019-01-15 15:04:51 +0000

    base controller now has debug variable and named keywords in constructor

--- controllers/base.controller.js
+++ controllers/base.controller.js
@@ -11,11 +11,13 @@
 const _ = require("underscore");
 
 class baseController {
-    constructor(name){
+    constructor(name = "Base Controller",debug = false){
         this.name = name;
+        this.debug = debug;
     }
 
     run(req,res,next){
+        if (this.debug) console.log(`Running 'run' from ${this.name}`)
         if (!req | !res | !next) {
             throw new Error("Parameters are wrong")
         }
