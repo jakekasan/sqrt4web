5fafaa2b2017208b4f1c879f676915c280a49fae
jakekasan <jake.kasan@gmail.com>
2019-02-04 19:50:55 +0000

    added console.logs for fillModal

--- public/js/modal.js
+++ public/js/modal.js
@@ -12,8 +12,12 @@ function windowOnClick(event) {
     }
 }
 
-function fillModal(target){
-    let data = _cachedData.filter(item => item.id == target.dataset.id)[0];
+function fillModal(id){
+    let data = _cachedData.filter(item => item.id == id)[0];
+    console.log(_cachedData);
+    console.log(data);
+
+    if (!data) return
 
     let courseElement = modal.querySelector(".course");
 
@@ -21,19 +25,24 @@ function fillModal(target){
 
     let title = document.createElement("div");
     title.classList.add("title");
+    title.innerText = data.title;
 
     let description = document.createElement("div");
-    title.classList.add("description");
+    description.classList.add("description");
+    description.innerText = data.fullDesc;
 
-    data.modules.forEach(item => {
+    data.modules.forEach((item) => {
+        console.log(item);
         let icon = document.createElement("div");
         icon.classList.add("module-icon");
 
         let text = document.createElement("div");
         text.classList.add("module-text");
+        text.innerText = item.title;
 
         let minutes = document.createElement("div");
         minutes.classList.add("module-minutes");
+        minutes.innerText = item.minutes;
 
         let moduleChoice = document.createElement("div");
         moduleChoice.classList.add("module-choice");
@@ -42,10 +51,23 @@ function fillModal(target){
         moduleChoice.appendChild(text);
         moduleChoice.appendChild(minutes);
 
-        courseElement.appendChild
-
-
+        courseElement.appendChild(moduleChoice);
     });
+
+    let buttonModuleContainer = document.createElement("div");
+    buttonModuleContainer.classList.add("module-button-container");
+
+    let buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("button");
+
+    let button = document.createElement("button");
+    button.type = "submit";
+    button.innerText = "SPUSTIT";
+
+    buttonContainer.appendChild(button);
+    buttonModuleContainer.appendChild(buttonContainer);
+
+    courseElement.appendChild(buttonModuleContainer);
 }
 
 trigger.addEventListener("click",toggleModal);
