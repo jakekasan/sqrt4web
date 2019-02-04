79b6d5f4cd38928dc15e5b651b623d9bbfd18e2a
jakekasan <jake.kasan@gmail.com>
2019-02-04 19:22:38 +0000

    added fillModal function (WIP)

--- public/js/modal.js
+++ public/js/modal.js
@@ -12,6 +12,42 @@ function windowOnClick(event) {
     }
 }
 
+function fillModal(target){
+    let data = _cachedData.filter(item => item.id == target.dataset.id)[0];
+
+    let courseElement = modal.querySelector(".course");
+
+    courseElement.innerHTML = "";
+
+    let title = document.createElement("div");
+    title.classList.add("title");
+
+    let description = document.createElement("div");
+    title.classList.add("description");
+
+    data.modules.forEach(item => {
+        let icon = document.createElement("div");
+        icon.classList.add("module-icon");
+
+        let text = document.createElement("div");
+        text.classList.add("module-text");
+
+        let minutes = document.createElement("div");
+        minutes.classList.add("module-minutes");
+
+        let moduleChoice = document.createElement("div");
+        moduleChoice.classList.add("module-choice");
+
+        moduleChoice.appendChild(icon);
+        moduleChoice.appendChild(text);
+        moduleChoice.appendChild(minutes);
+
+        courseElement.appendChild
+
+
+    });
+}
+
 trigger.addEventListener("click",toggleModal);
 closeButton.addEventListener("click",toggleModal);
 window.addEventListener("click",windowOnClick);
