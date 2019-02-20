31ea7f50f08e9d716efac5559fe26ae7c8aa03f0
jakekasan <jake.kasan@gmail.com>
2019-02-20 10:51:21 +0000

    added findOne and findAll to projects model

--- models/projects.model.js
+++ models/projects.model.js
@@ -1,5 +1,7 @@
 const BaseModel = require("./base.model");
 const projectsSchema = require("./schemas/projects.schema");
+const path = require("path");
+const fs = require("fs");
 
 fakeConnection = {
     model: function(string,schema) {
@@ -17,7 +19,32 @@ class ProjectsModel extends BaseModel {
             connection = fakeConnection;
         }
         let model = connection.model("Projects",projectsSchema);
-        super(model)
+        super(model);
+    }
+
+    findOne(obj,callback){
+        // for now, load data from file
+        let { id } = obj;
+
+        let dataPath = path.resolve(__dirname,"../public",'projects.json');
+
+        var data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+
+        data = data.filter(item => item.id == id)[0];
+
+        return callback(data)
+    }
+
+    findAll(obj,callback){
+        let { id } = obj;
+
+        let dataPath = path.resolve(__dirname,"../public",'projects.json');
+
+        var data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+
+        // data = data.filter(item => item.id == id)[0];
+
+        return callback(data)
     }
 }
 
