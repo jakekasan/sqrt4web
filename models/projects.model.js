787695a6ceb02964d7756b1a2cc6ed038615f5a0
jakekasan <jake.kasan@gmail.com>
2019-02-26 20:16:07 +0000

    both models now use mongoose methods instead of fs

--- models/projects.model.js
+++ models/projects.model.js
@@ -1,24 +1,16 @@
 const BaseModel = require("./base.model");
 const projectsSchema = require("./schemas/projects.schema");
-const path = require("path");
-const fs = require("fs");
 
-fakeConnection = {
-    model: function(string,schema) {
-        return {
-            string,
-            schema
-        }
-    }
-}
+let fakeConnection = require("./fake.connection");
 
 class ProjectsModel extends BaseModel {
     constructor(connection) {
         var connection = connection;
         if (!connection) {
-            connection = fakeConnection;
+            console.log("Setting filename for Projects Model");
+            connection = fakeConnection("projects.json");
         }
-        let model = connection.model("Projects",projectsSchema);
+        let model = connection.model("Project",projectsSchema);
         super(model);
     }
 
@@ -26,25 +18,36 @@ class ProjectsModel extends BaseModel {
         // for now, load data from file
         let { id } = obj;
 
-        let dataPath = path.resolve(__dirname,"../public",'projects.json');
+        // let dataPath = path.resolve(__dirname,"../public",'projects.json');
 
-        var data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
-
-        data = data.filter(item => item.id == id)[0];
-
-        return callback(data)
-    }
-
-    findAll(obj,callback){
-        let { id } = obj;
-
-        let dataPath = path.resolve(__dirname,"../public",'projects.json');
-
-        var data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+        // var data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
 
         // data = data.filter(item => item.id == id)[0];
 
-        return callback(data)
+        this.retrieve(obj,(err,data) => {
+            if (err) {
+                console.log(err);
+            }
+            return callback(data)
+        })
+        
+    }
+
+    findAll(obj,callback){
+        // let { id } = obj;
+
+        // let dataPath = path.resolve(__dirname,"../public",'projects.json');
+
+        // var data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+
+        // data = data.filter(item => item.id == id)[0];
+
+        this.retrieve(obj,(err,data) => {
+            if (err) {
+                console.log(err);
+            }
+            return callback(data)
+        })
     }
 }
 
--- models/users.model.js
+++ models/users.model.js
@@ -1,25 +1,46 @@
 const BaseModel = require("./base.model");
 const userSchema = require("./schemas/users.schema");
 
-var fakeConnection = {
-    model: function(string,schema){
-        return {
-            string,
-            schema
-        }
-    }
-}
-
+var fakeConnection = require("./fake.connection");
 
 class UsersModel extends BaseModel {
 
     constructor(connection){
         var connection = connection;
         if (!connection){
-            connection = fakeConnection;
+            console.log("Setting filename for Users Model");
+            connection = fakeConnection("users.json");
         }
-        let model = connection.model("Users",userSchema);
+        let model = connection.model("User",userSchema);
         super(model);
+    }
+
+    findOne(obj,callback){
+        let { id } = obj;
+
+        let dataPath = path.resolve(__dirname,"../public",'users.json');
+
+        var data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+
+        data = data.filter(item => item.id == id)[0];
+
+        return callback(data)
+    }
+
+    findAll(obj,callback){
+        // let { id } = obj;
+
+        let dataPath = path.resolve(__dirname,"../public",'users.json');
+
+        var data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+
+        data = data.filter(item => id.includes(item.id));
+
+        return callback(data)
+    }
+
+    createNew(obj,callback){
+
     }
 }
 
