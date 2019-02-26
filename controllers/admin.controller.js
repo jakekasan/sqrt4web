e68c3cbaf77f37404e0632dcf85e58d9144b9c96
jakekasan <jake.kasan@gmail.com>
2019-02-26 13:36:59 +0000

    admin controller to handle editing content

---
+++ controllers/admin.controller.js
@@ -0,0 +1,48 @@
+const BaseController = require("./base.controller");
+const UsersModel = require("./../models/users.model");
+const BaseView = require("./../views/base.view");
+const CoursesService = require("./services/courses.service");
+
+module.exports = class LoginController extends BaseController {
+    constructor(connection){
+        let model = new UsersModel(connection);
+        super(model);
+    }
+
+    run(req,res,next){
+        let self = this;
+        self.req = req;
+        self.res = res;
+        self.next = next;
+
+        if (self.paths[self.req.path] && self.paths[self.req.path][self.req.method]){
+            return self.paths[self.req.path][self.req.method](self);
+        } else {
+            return next();
+        }
+    }
+
+    get paths(){
+        return {
+            "/admin/course":{
+                "GET":(self) => {
+                    // 
+                    let view = new BaseView("login",self.res);
+
+                    return view.render({
+                        message:null
+                    });
+                },
+                "POST":(self) => {
+                    // handle post
+
+                    let view = new BaseView("login",self.res);
+                    
+                    return view.render({
+                        message:JSON.stringify(self.body)
+                    });
+                }
+            }
+        }
+    }
+}
