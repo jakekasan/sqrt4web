4b11fbe9810bf7f615e4570ead1a31240d00acac
jakekasan <jake.kasan@gmail.com>
2019-02-26 20:15:15 +0000

    now uses mongoose methods instead of fs

--- models/lessons.model.js
+++ models/lessons.model.js
@@ -4,57 +4,74 @@ const fetch = require("node-fetch");
 var fs = require("fs");
 var path = require("path");
 
-
-
-let fakeData = require("./../public/courses.json");
-
-var fakeConnection = {
-    model: function(string,schema){
-        return {
-            string,
-            schema
-        }
-    }
-}
+var fakeConnection = require("./fake.connection")
 
 class LessonsModel extends BaseModel {
     constructor(connection){
         var connection = connection;
         if (!connection){
-            connection = fakeConnection;
+            console.log("Setting filename for Lessons Model");
+            connection = fakeConnection("lessons.json");
         }
-        let model = connection.model("Lessons",lessonsSchema);
-        super(model)
+        let model = connection.model("Lesson",lessonsSchema);
+        super(model);
     }
 
     getData(callback){
-        let dataPath = path.resolve(__dirname,"../public",'lessons.json');
-        console.log(dataPath);
-        var obj = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+        // let dataPath = path.resolve(__dirname,"../public",'lessons.json');
+        // console.log(dataPath);
+        // var obj = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
         
-        return callback(obj)
+        this.retrieve({},(err,data) => {
+            if (err) {
+                console.log(err);
+            }
+            return callback(data)
+        })
     }
 
     findOne(obj,callback){
-        let { id } = obj;
+        // let { id } = obj;
 
-        let dataPath = path.resolve(__dirname,"../public",'lessons.json');
+        // let dataPath = path.resolve(__dirname,"../public",'lessons.json');
 
-        var data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+        // var data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
 
-        data = data.filter(item => item.id == id)[0];
+        // data = data.filter(item => item.id == id)[0];
 
-        return callback(data)
+        this.retrieve(obj,(err,data) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log("\n\n");
+            console.log("findOne in Lessons Model");
+            console.log(obj);
+            console.log("result:");
+            console.log(data);
+            console.log("\n\n");
+            let result = data.pop();
+
+            return callback(result,null)
+        })
+
+        // return callback(data)
     }
 
     findAll(obj,callback){
-        let { id } = obj;
+        // let { id } = obj;
 
-        let dataPath = path.resolve(__dirname,"../public",'lessons.json');
+        // let dataPath = path.resolve(__dirname,"../public",'lessons.json');
 
-        var data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+        // var data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
 
-        data = data.filter(item => id.includes(item.id));
+        // data = data.filter(item => id.includes(item.id));
+
+        this.retrieve(obj,(err,data) => {
+            if (err) {
+                console.log(err);
+            }
+            return callback(data)
+        })
 
         return callback(data)
     }
