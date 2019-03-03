fc5204ac3b6e7425a7c84187cb33399e80734e75
jakekasan <jake.kasan@gmail.com>
2019-03-03 17:41:06 +0000

    segmented edit and delete listeners and listItem creation

--- public/js/edit.js
+++ public/js/edit.js
@@ -6,6 +6,41 @@ document.addEventListener("DOMContentLoaded",() => {
 
     let addLessonButton = document.querySelector("#addLessonButton");
 
+
+    // add listeners for existing list item
+
+    let lessonsList = document.querySelector("#lessons");
+    let projectsList = document.querySelector("#projects");
+
+    console.log("items");
+    let lessons = Array.from(lessonsList.children);
+    lessons.map(item => {
+        let buttons = item.querySelector(".item-buttons");
+        let editButton = buttons.children[0];
+        let deleteButton = buttons.children[1];
+        
+        editButton.addEventListener("click",(event) => {
+            editButtonClickEvent({
+                event:event,
+                listItem:item
+            });
+        })
+
+        deleteButton.addEventListener("click", (event) => {
+            deleteButtonClickEvent({
+                event: event,
+                listItem:item
+            })
+        })
+
+    })
+    // lessonsList.children.forEach(child => {
+    //     let editButton = child.querySelectorAll(".container-buttons");
+    // })
+
+    // console.log(lessonsList.children);
+    // console.log(projectsList.children);
+
     addProjectButton.addEventListener("click",(event) => {
         // ASYNC load project list into modal
         console.log("addProject Clicked");
@@ -119,37 +154,12 @@ function doModal(params){
 
             // console.log(list);
 
-            let containerListItem = document.createElement("div");
-            containerListItem.classList.add("container-list-item");
-
-            let listItemTitle = document.createElement("div");
-            listItemTitle.classList.add("item-title")
-            listItemTitle.innerText = dataItem.title;
-
-            let buttonsContainer = document.createElement("div");
-            buttonsContainer.classList.add("item-buttons");
-
-            // EDIT BUTTON
-            let editButton = document.createElement("div");
-            editButton.classList.add("button");
-            editButton.innerText = "Edit";
-
-            // DELETE BUTTON
-            let deleteButton = document.createElement("div");
-            deleteButton.classList.add("button");
-            deleteButton.innerText = "Delete";
-
-            // HIDDEN INPUT
-            let hiddenInput = document.createElement("input");
-            hiddenInput.type = "hidden";
-            hiddenInput.name = `${type}s[]`
-            hiddenInput.value = dataItem.id;
-
-            buttonsContainer.appendChild(editButton);
-            buttonsContainer.appendChild(deleteButton);
-
-            containerListItem.appendChild(listItemTitle);
-            containerListItem.appendChild(buttonsContainer);
+            let containerListItem = createLessonItem({
+                title: dataItem.title,
+                id: dataItem.id,
+                type: type,
+                list: list
+            })
 
             list.appendChild(containerListItem);
 
@@ -164,3 +174,76 @@ function doModal(params){
     modal.classList.add("show-modal");
 }
 
+function createLessonItem(params){
+    let { title, id, type, list } = params;
+
+    // create parent list item
+    let containerListItem = document.createElement("div");
+    containerListItem.classList.add("container-list-item");
+
+    // title element
+    let listItemTitle = document.createElement("div");
+    listItemTitle.classList.add("item-title")
+    listItemTitle.innerText = title;
+
+    let buttonsContainer = document.createElement("div");
+    buttonsContainer.classList.add("item-buttons");
+
+    // EDIT BUTTON
+    let editButton = document.createElement("div");
+    editButton.classList.add("button");
+    editButton.innerText = "Edit";
+    editButton.addEventListener("click",(event) => {
+        editButtonClickEvent({
+            event: event,
+            listItem: containerListItem
+        });
+    })
+
+    // DELETE BUTTON
+    let deleteButton = document.createElement("div");
+    deleteButton.classList.add("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.addEventListener("click",(event) => {
+        deleteButtonClickEvent({
+            event: event,
+            listItem: containerListItem
+        });
+    })
+
+    // HIDDEN INPUT
+    let hiddenInput = document.createElement("input");
+    hiddenInput.type = "hidden";
+    hiddenInput.name = `${type}s[]`
+    hiddenInput.value = id;
+
+    buttonsContainer.appendChild(editButton);
+    buttonsContainer.appendChild(deleteButton);
+
+    containerListItem.appendChild(listItemTitle);
+    containerListItem.appendChild(buttonsContainer);
+
+    return containerListItem
+}
+
+function editButtonClickEvent(params){
+    let { event, listItem } = params;
+    let target = event.target;
+    while (!target.classList.contains("container-list-item")){
+        target = target.parentNode;
+    }
+    console.log("Clicked edit button for",target);
+}
+
+function deleteButtonClickEvent(params){
+    let { event, listItem } = params;
+    let target = event.target;
+    console.log(target.classList);
+    console.log("Checking",target);
+    while (!(target.classList.contains("container-list-item"))){
+        target = target.parentNode;
+        console.log("Checking",target);
+    }
+
+    (listItem.parentNode).removeChild(listItem);
+}
